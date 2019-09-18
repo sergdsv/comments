b84221db8bd38836b749bec4109288f63787bc69
sergdsv <serg.dsv@gmail.com>
2019-09-18 14:35:39 +0300

    dsv

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <button @click="getArray">sfdwe</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comment from './components/Comment.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Comment
+  },
+  data() {
+    return {
+      comments: [],
+      arr: [
+            {id: 1, title: 'hello1', parent: 0},
+            {id: 2, title: 'hello2', parent: 0},
+            {id: 3, title: 'hello3', parent: 1},
+            {id: 4, title: 'hello4', parent: 1},
+            {id: 5, title: 'hello5', parent: 4},
+            {id: 6, title: 'hello6', parent: 4},
+            {id: 7, title: 'hello7', parent: 3},
+            {id: 8, title: 'hello8', parent: 2}
+      ],
+      arr_out: [],
+      // parrent: ''
+    }
+  },
+
+  methods: {
+            getNested(arr) {
+                const output = []
+                 arr.forEach(i => {
+                   if (i.parent == 0){
+                      this.arr_out.push(i)
+                      const parent = i.id
+                        arr.forEach(i => {
+                          if(i.parent == parent){
+                            this.arr_out.push(i.children = i)
+                          }
+                        })
+                   }
+                   
+                 });
+            },
+
+
+            getArray: function() {
+                this.getNested(this.arr)
+            }
+        },
+
+
+  mounted() {
+        
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Comment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
